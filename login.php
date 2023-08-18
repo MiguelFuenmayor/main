9005b2c8b16c39c7f1f8835b35a06b1bf9f6990c
Miguel Fuenmayor <miguelfuenmayormn@gmail.com>
2023-08-18 16:56:55 -0400

    elminar login

--- login.php
+++
@@ -1,5 +0,0 @@
-<form method="POST" action="back/login_registro.php">
-    <input placeholder="mail" type="text" name="mail">
-    <input type="password" name="contrasena">
-    <input type="submit" value="Iniciar Sesión">
-</form>
